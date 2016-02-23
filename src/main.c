c5070164390c7a0176ab051b8ebf5f2b8ec36992
joseph-aylor <joseph.aylor@gmail.com>
2016-02-23 15:51:07 -0500

    Hello World!

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+static TextLayer *s_time_layer;
 
 static void main_window_load(Window *window) {
-
+  Layer * window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  
+  s_time_layer = text_layer_create(
+    //GRect(0, 52, bounds.size.w, 50));
+    GRect(0, 0, bounds.size.w, bounds.size.h));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "H\nello\nWorld\n!!!!");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
